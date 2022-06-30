4beed47722a2c1ddb04f0ba47a8fc9a76705343d
Sherkhan Tulkibay <sherkhan-92@bk.ru>
2022-06-30 22:42:55 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+import Item from './components/item';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [text, setText] = useState('');
+    const [task, setTask] = useState([]);
+    const [isUpdating, setUpdating] = useState('');
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/get-task')
+            .then((res) => setTask(res.data))
+            .catch((err) => console.log(err))
+    })
+
+    const addUpdate = () => {
+        if(isUpdating === '') {
+            axios.post('http://localhost:5000/save-task', {text})
+                .then((res) => {
+                    console.log(res.data)
+                    setText('');
+                })
+                .catch((err) => console.log(err))
+        } else{
+            axios.post('http://localhost:5000/update-task', {_id: isUpdating, text})
+                .then((res) => {
+                    console.log(res.data)
+                    setText('');
+                    setUpdating('');
+                })
+                .catch((err) => console.log(err))
+        }
+    }
+
+    const deleteTask = (_id) => {
+        axios.post('http://localhost:5000/delete-task', {_id})
+            .then((res) => {
+                console.log(res.data);
+            })
+            .catch((err) => console.log(err))
+    }
+
+    const updateTask = (_id, text) => {
+        setUpdating(_id);
+        setText(text);
+    }
+
+    return (
+        <div className="App">
+            <div className="container">
+                <h1>Task</h1>
+
+                <div className="top">
+                    <input type="text"
+                           placeholder='Write Something...'
+                           value={text}
+                           onChange={(e) => setText(e.target.value)}/>
+                    <div className="add" onClick={addUpdate}>{isUpdating ? 'Update' : 'Add'}</div>
+                </div>
+                <div className="list">
+                    {task.map(item => <Item
+                        key={item._id}
+                        text={item.text}
+                        remove={() => deleteTask(item._id)}
+                        update={() => updateTask(item._id, item.text)} />)}
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
